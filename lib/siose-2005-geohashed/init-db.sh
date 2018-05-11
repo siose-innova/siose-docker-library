f06f37f9622a228d853dcd26b32026c026d021e4
benizar <benizar@gmail.com>
2018-05-11 14:42:37 +0200

    restore database

--- lib/siose-2005-geohashed/init-db.sh
+++ lib/siose-2005-geohashed/init-db.sh
@@ -5,22 +5,34 @@ set -e
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
-echo "Load extensions into $POSTGRES_DB"
+echo "Setup $POSTGRES_DB"
 for DB in "$POSTGRES_DB"; do
 
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
 
 		CREATE EXTENSION IF NOT EXISTS postgis;
 		CREATE EXTENSION IF NOT EXISTS pg_geohash_extra;
+		CREATE SCHEMA s2005;
+		CREATE SCHEMA s2011;
 
 EOSQL
 done
 
-
+#############
+# Load data #
+#############
 
 # Import sample data
 ogr2ogr -f "PostgreSQL" "PG:dbname=db schemas=public user=postgres password=postgres" "/data/castellon.shp" -lco GEOMETRY_NAME=geom -lco FID=gid -lco PRECISION=no -nlt PROMOTE_TO_MULTI -nln roi -overwrite
 
+
+# Now restore using concurrent import
+pg_restore -d postgresql://postgres:postgres@localhost/db -j 3 -O -x /data/dump2005-castellon
+
+
+################
+# Prepare data #
+################
 
 # Create a geohashes table from roi
 "${psql[@]}" --dbname="$POSTGRES_DB" <<-'EOSQL'
--- lib/siose-2005/init-db.sh
+++ lib/siose-2005/init-db.sh
@@ -10,11 +10,15 @@ for DB in "$POSTGRES_DB"; do
 
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS postgis;
+		CREATE SCHEMA s2005;
+		CREATE SCHEMA s2011;
 EOSQL
 done
 
 # Restore backup
 ogr2ogr -f "PostgreSQL" "PG:dbname=db schemas=public user=postgres password=postgres" "/data/castellon.shp" -lco GEOMETRY_NAME=geom -lco FID=gid -lco PRECISION=no -nlt PROMOTE_TO_MULTI -nln roi -overwrite
 
+
+
 # Now restore using concurrent import
-#pg_restore -j 8 -U postgres -d db /data/dump2005-castellon.dump
+pg_restore -d postgresql://postgres:postgres@dbm/db -j 3 -O -x /data/dump2005-castellon
