de4f5ff8cdad0de0ef07e7c641b4d3b2d0fcd94f
benizar <benizar@gmail.com>
2018-05-29 20:08:48 +0200

    shp2pgsql instead of ogr2ogr

--- lib/siose-2005/base/init-db.sh
+++ lib/siose-2005/base/init-db.sh
@@ -15,10 +15,14 @@ for DB in "$POSTGRES_DB"; do
 EOSQL
 done
 
-# Restore backup
-ogr2ogr -f "PostgreSQL" "PG:dbname=db schemas=public user=postgres password=postgres" "/data/castellon.shp" -lco GEOMETRY_NAME=geom -lco FID=gid -lco PRECISION=no -nlt PROMOTE_TO_MULTI -nln roi -overwrite
+#############
+# Load data #
+#############
 
+# Import sample data
+#ogr2ogr -f "PostgreSQL" "PG:dbname=db schemas=public user=postgres password=postgres" "/data/castellon.shp" -lco GEOMETRY_NAME=geom -lco FID=gid -lco PRECISION=no -nlt PROMOTE_TO_MULTI -nln roi -overwrite
+shp2pgsql -s 4326 /data/castellon.shp public.roi | psql -d db -U postgres
 
 
 # Now restore using concurrent import
-pg_restore -d postgresql://postgres:postgres@dbm/db -j 3 -O -x /data/dump2005-castellon
+pg_restore -d db -U postgres -j 3 -O -x /data/dump2005-castellon
